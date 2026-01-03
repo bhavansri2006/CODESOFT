588478f909df2c7af6cce899b8fa3b60f4dd5ea0
bhavansri2006 <bhavansri.m@gmail.com>
2026-01-03 10:26:00 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,8 +1,1 @@
-function add(value) {
-    document.getElementById("display").value += value;
-}
 
-function calculate() {
-    let result = document.getElementById("display").value;
-    document.getElementById("display").value = eval(result);
-}
